a0925254b9610a46afe618970c916a91d4dab9cc
Ted <ted@dhcp-206-12-67-110.ubcsecure.wireless.ubc.ca>
2018-04-12 15:10:42 -0700

    ted final test

--- tests/testthat/test-cloudmap.R
+++ tests/testthat/test-cloudmap.R
@@ -2,15 +2,14 @@ context("Testing distmap")
 
 api <- '02c3680b276051044255784768becd85'
 a <- content(GET(paste0("http://api.openweathermap.org/data/2.5/weather?q=toronto&APPID=",api)))
-city1 <- 'vancouver'
-city2 <- 'toronto'
+city <- 'toronto'
 
 # This test validates the api
 test_that("Validate api", {
   expect_equal(a$name,"Toronto")
 })
 
-# This test the distance is correct
-test_that("Check distance between cities", {
-  expect_equal(distmap(api,city1,city2),"The distance between vancouver and toronto is 3398900.0286213 km")
+# This test the plot
+test_that("Check cloudmap", {
+  expect_equal(typeof(cloudmap(api, city, dense = 0.2 )),"list")
 })
